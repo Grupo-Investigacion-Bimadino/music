66bbc2b80b5ac115aa30cccde1826119765e4dfa
Alexander Toscano Ricardo <atoscano@correo.unicordoba.edu.co>
2024-06-04 06:36:00 -0500

    Modules Creation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ArtistModule } from './artist/artist.module';
+import { SongsModule } from './songs/songs.module';
+import { ListModule } from './list/list.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [ArtistModule, SongsModule, ListModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
